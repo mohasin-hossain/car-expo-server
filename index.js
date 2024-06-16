467d4800fad3995c12f563da4298fb460d525c9b
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-16 21:53:38 +0600

    Car Expo complete crud

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x53s3dr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,25 +25,89 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const productsCollection = client
+      .db("productsDatabase")
+      .collection("products");
+    const cartsCollection = client.db("productsDatabase").collection("carts");
 
+    app.get("/products/:brand", async (req, res) => {
+      const brand = req.params.brand;
+      const query = { brandName: brand };
+      const cursor = productsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    app.get("/product-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
 
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
 
+    app.get("/cart", async (req, res) => {
+      const cursor = cartsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    app.post("/cart", async (req, res) => {
+      const addedProduct = req.body;
+      const { productName } = addedProduct;
+      const query = { productName: productName };
+      const found = await cartsCollection.findOne(query);
+      if (!found) {
+        const result = await cartsCollection.insertOne(addedProduct);
+        res.send(result);
+      } else {
+        res.send("Already In the Cart");
+      }
+    });
 
+    app.put("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const product = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = {
+        $set: {
+          photo: product.photo,
+          productName: product.productName,
+          brandName: product.brandName,
+          productType: product.productType,
+          price: product.price,
+          description: product.description,
+          rating: product.rating,
+        },
+      };
+      const result = await productsCollection.updateOne(
+        filter,
+        updatedProduct,
+        options
+      );
+      res.send(result);
+    });
 
-
-
-
-
-
-
-
-
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: id };
+      console.log(query);
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -52,11 +115,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send("Car Expo Server is running..,")
-})
+app.get("/", (req, res) => {
+  res.send("Car Expo Server is running..,");
+});
 
 app.listen(port, () => {
-    console.log(`Listening on ${port}...`)
-})
+  console.log(`Listening on ${port}...`);
+});
